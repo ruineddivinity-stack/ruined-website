abd27d4349171647d21524e82a61b4c2b0e569fd
Shawn <ruineddivinity@gmail.com>
2026-08-18 19:51:48 -0500

    Remove debug from-address suffix from Resend error messages

--- lib/resend.ts
+++ lib/resend.ts
@@ -65,7 +65,7 @@ export async function sendBroadcastEmail({
   }
 
   if (sent === 0 && failed > 0 && lastError) {
-    throw new Error(`${lastError} (from="${FROM_EMAIL}")`);
+    throw new Error(lastError);
   }
 
   return { sent, failed };
